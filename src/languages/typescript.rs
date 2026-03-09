ccde1011f23d54f28666c056ca0900d49c096ce2
LeonPuchinger <lepu1590@gmail.com>
2026-03-09 12:36:18 +0100

    add an example parser for typescript function definitions

--- src/languages/typescript.rs
+++ src/languages/typescript.rs
@@ -1,7 +1,19 @@
 use crate::{
     config::{FeatureParser, LanguageConfig, LanguageFeature},
-    parser::lexer::LazyLexer,
+    parser::{
+        ParserError,
+        atoms::{exact, token_kind},
+        lexer::{self, LazyLexer},
+    },
 };
+
+fn keyworded_function_definition(lexer: &mut dyn lexer::Lexer) -> Result<(), ParserError> {
+    let _keyword = exact("function")(lexer)?;
+    let _name = token_kind("identifier")(lexer)?;
+    let _open_parenthesis = exact("(")(lexer)?;
+    let _close_parenthesis = exact(")")(lexer)?;
+    Ok(())
+}
 
 pub fn register<'a>() -> LanguageConfig<'a> {
     // Dummy config for TS, just for demonstration purposes of the overall architecture.
