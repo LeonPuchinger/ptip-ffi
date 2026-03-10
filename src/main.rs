209087493f9469b707059ff6534786e61f0d707e
LeonPuchinger <lepu1590@gmail.com>
2026-03-10 16:38:45 +0100

    remove no longer needed example code

--- src/main.rs
+++ src/main.rs
@@ -14,11 +14,3 @@ fn main() {
         println!("Language found: {}", lang.name);
     }
 }
-
-// example parser usage
-pub fn custom_parser<'a>(lexer: &mut dyn Lexer<'a>) -> Result<String, ParserError> {
-    let identifier = token_kind("Identifier")(lexer)?;
-    let _assignment_operator = token_kind("AssignmentOperator")(lexer)?;
-    let number = token_kind("Number")(lexer)?;
-    Ok(format!("Parsed assignment: {} = {}", identifier, number))
-}
